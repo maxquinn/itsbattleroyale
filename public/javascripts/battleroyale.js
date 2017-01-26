2817e0c1cf6969b4200694fed8db823021364098
Max Quinn <quinnma@staff.vuw.ac.nz>
2017-01-26 16:58:03 +1300

    added more functions to game core

--- public/javascripts/battleroyale.js
+++ public/javascripts/battleroyale.js
@@ -24,6 +24,15 @@ var game_core = function (game_instance) {
     this.playerspeed = 120; //speed that players move
 
     //physics integration values:
+    this._pdt = 0.0001;                 //The physics update delta time
+    this._pdte = new Date().getTime();  //The physics update last delta time
+    //A local timer for precision on server and client
+    this.local_time = 0.016;            //The local timer
+    this._dt = new Date().getTime();    //The local timer delta
+    this._dte = new Date().getTime();   //The local timer last frame time
+
+    this.create_physics_simulation();
+    this.create_timer();
 
     //client and server initialisation
     if (!this.server) {
@@ -31,6 +40,48 @@ var game_core = function (game_instance) {
         this.server_updates = [];
         this.client_connect_to_server();
     }
+
+};
+
+/*
+    Helper functions for the game code
+        Here we have some common maths and game related code to make working with 2d vectors easy,
+        as well as some helpers for rounding numbers to fixed point.
+*/
+
+// (4.22208334636).fixed(n) will return fixed point value to n places, default n = 3
+Number.prototype.fixed = function (n) { n = n || 3; return parseFloat(this.toFixed(n)); };
+//copies a 2d vector like object from one to another
+game_core.prototype.pos = function (a) { return { x: a.x, y: a.y }; };
+//Add a 2d vector with another one and return the resulting vector
+game_core.prototype.v_add = function (a, b) { return { x: (a.x + b.x).fixed(), y: (a.y + b.y).fixed() }; };
+//Subtract a 2d vector with another one and return the resulting vector
+game_core.prototype.v_sub = function (a, b) { return { x: (a.x - b.x).fixed(), y: (a.y - b.y).fixed() }; };
+//Multiply a 2d vector with a scalar value and return the resulting vector
+game_core.prototype.v_mul_scalar = function (a, b) { return { x: (a.x * b).fixed(), y: (a.y * b).fixed() }; };
+//For the server, we need to cancel the setTimeout that the polyfill creates
+game_core.prototype.stop_update = function () { window.cancelAnimationFrame(this.updateid); };
+//Simple linear interpolation
+game_core.prototype.lerp = function (p, n, t) { var _t = Number(t); _t = (Math.max(0, Math.min(1, _t))).fixed(); return (p + _t * (n - p)).fixed(); };
+//Simple linear interpolation between 2 vectors
+game_core.prototype.v_lerp = function (v, tv, t) { return { x: this.lerp(v.x, tv.x, t), y: this.lerp(v.y, tv.y, t) }; };
+
+
+game_core.prototype.create_timer = function () {
+    setInterval(function () {
+        this._dt = new Date().getTime() - this._dte;
+        this._dte = new Date().getTime();
+        this.local_time += this._dt / 1000.0;
+    }.bind(this), 4);
+}
+
+game_core.prototype.create_physics_simulation = function () {
+
+    setInterval(function () {
+        this._pdt = (new Date().getTime() - this._pdte) / 1000.0;
+        this._pdte = new Date().getTime();
+        this.update_physics();
+    }.bind(this), 15);
 
 };
 
@@ -78,7 +129,15 @@ game_core.prototype.client_connect_to_server = function () {
 
     this.socket.on('disconnect', this.client_ondisconnect.bind(this));
     this.socket.on('onserverupdate', this.client_onserverupdate_recieved.bind(this));
+    this.socket.on('onconnected', this.client_onconnected.bind(this));
 };
+
+game_core.prototype.client_onconnected = function() {
+    //server responds that we are now in a game
+    this.players.self.id = data.id;
+    this.players.self.state = 'connected';
+    this.players.self.online = true;
+}
 
 game_core.prototype.client_ondisconnect = function (data) {
     this.players.self.state = 'not-connected';
@@ -106,8 +165,127 @@ game_core.prototype.client_onserverupdate_recieved = function (data) {
     this.client_process_net_prediction_correction();
 };
 
-game_core.prototype.client_process_net_prediction_correction = function() {
-    if(!this.server_updates.length) return;
+game_core.prototype.client_process_net_prediction_correction = function () {
+    if (!this.server_updates.length) return;
+
+    var latest_server_data = this.server_updates[this.server_updates.length - 1];
+
+    var my_server_pos = this.players.self.host ? latest_server_data.hp : latest_server_data.cp;
+
+    var my_last_input_on_server = this.players.self.host ? latest_server_data.his : latest_server_data.cis;
+
+    if (my_last_input_on_server) {
+        var lastinputseq_index = -1;
+
+        for (var i = 0; i < this.players.self.inputs.length; ++i) {
+            if (this.players.self.inputs[i].seq == my_last_input_on_server) {
+                lastinputseq_index = i;
+                break;
+            }
+        }
+
+        if (lastinputseq_index != -1) {
+            var number_to_clear = Math.abs(lastinputseq_index - (-1));
+            this.players.self.inputs.splice(0, number_to_clear);
+            //The player is now located at the new server position, authoritive server
+            this.players.self.cur_state.pos = this.pos(my_server_pos);
+            this.players.self.last_input_seq = lastinputseq_index;
+            //Now we reapply all the inputs that we have locally that
+            //the server hasn't yet confirmed. This will 'keep' our position the same,
+            //but also confirm the server position at the same time.
+            this.client_update_physics();
+            this.client_update_local_position();
+        }
+    }
+};
+
+game_core.prototype.client_update_local_position = function () {
+    if (this.client_predict) {
+        //Work out the time we have since we updated the state
+        var t = (this.local_time - this.players.self.state_time) / this._pdt;
+
+        //Then store the states for clarity,
+        var old_state = this.players.self.old_state.pos;
+        var current_state = this.players.self.cur_state.pos;
+
+        //Make sure the visual position matches the states we have stored
+        //this.players.self.pos = this.v_add( old_state, this.v_mul_scalar( this.v_sub(current_state,old_state), t )  );
+        this.players.self.pos = current_state;
+
+        //We handle collision on client if predicting.
+        this.check_collision(this.players.self);
+    }
+};
+
+game_core.prototype.client_update_physics = function () {
+
+    //Fetch the new direction from the input buffer,
+    //and apply it to the state so we can smooth it in the visual state
+
+    if (this.client_predict) {
+
+        this.players.self.old_state.pos = this.pos(this.players.self.cur_state.pos);
+        var nd = this.process_input(this.players.self);
+        this.players.self.cur_state.pos = this.v_add(this.players.self.old_state.pos, nd);
+        this.players.self.state_time = this.local_time;
+
+    }
+
+};
+
+game_core.prototype.process_input = function (player) {
+    //It's possible to have recieved multiple inputs by now,
+    //so we process each one
+    var x_dir = 0;
+    var y_dir = 0;
+    var ic = player.inputs.length;
+    if (ic) {
+        for (var j = 0; j < ic; ++j) {
+            //don't process ones we already have simulated locally
+            if (player.inputs[j].seq <= player.last_input_seq) continue;
+
+            var input = player.inputs[j].inputs;
+            var c = input.length;
+            for (var i = 0; i < c; ++i) {
+                var key = input[i];
+                if (key == 'l') {
+                    x_dir -= 1;
+                }
+                if (key == 'r') {
+                    x_dir += 1;
+                }
+                if (key == 'd') {
+                    y_dir += 1;
+                }
+                if (key == 'u') {
+                    y_dir -= 1;
+                }
+            } //for all input values
+
+        } //for each input command
+    } //if we have inputs
+
+    //we have a direction vector now, so apply the same physics as the client
+    var resulting_vector = this.physics_movement_vector_from_direction(x_dir, y_dir);
+    if (player.inputs.length) {
+        //we can now clear the array since these have been processed
+
+        player.last_input_time = player.inputs[ic - 1].time;
+        player.last_input_seq = player.inputs[ic - 1].seq;
+    }
+
+    //give it back
+    return resulting_vector;
+};
+
+game_core.prototype.physics_movement_vector_from_direction = function (x, y) {
+
+    //Must be fixed step, at physics sync speed.
+    return {
+        x: (x * (this.playerspeed * 0.015)).fixed(3),
+        y: (y * (this.playerspeed * 0.015)).fixed(3)
+    };
+
 };
 
 var game_player = function (game_instance, player_instance) {
